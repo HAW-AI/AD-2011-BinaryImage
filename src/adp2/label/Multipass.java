714e79f8d48f5747b77450883da4801a6a2fb79d
panos <panagiotis.filippidis@haw-hamburg.de>
2011-10-31 19:14:22 +0100

    Einige Bugs in Multipass.java entfernt.

--- src/adp2/label/Multipass.java
+++ src/adp2/label/Multipass.java
@@ -7,14 +7,12 @@ public final class Multipass {
 
 	public static int[][] labelledMatrix4(int width, int height,
 			boolean[][] pixelMatrix) {
-		return labelledMatrix(width, height,
-				pixelMatrix, false);
+		return labelledMatrix(width, height, pixelMatrix, false);
 	}
-	
+
 	public static int[][] labelledMatrix8(int width, int height,
 			boolean[][] pixelMatrix) {
-		return labelledMatrix(width, height,
-				pixelMatrix, true);
+		return labelledMatrix(width, height, pixelMatrix, true);
 	}
 
 	private static int[][] labelledMatrix(int width, int height,
@@ -22,35 +20,36 @@ public final class Multipass {
 		// if (pixels.length == 0 || pixels[0].length == 0) {
 		// return null;
 		// }
-
-
+System.out.println("lol");
 		int[][] labels = new int[height][width];
+		System.out.println("lol");
 
-		//boolean diagonalNeighbours = true;
+		// boolean diagonalNeighbours = true;
 
 		int label = 1;
 		for (int y = 0; y < height; y++) {
 			for (int x = 0; x < width; x++) {
 				if (!pixelMatrix[y][x]) {
 					labels[y][x] = 0;
-					// System.out.println("x=" + x + " y=" + y + " label=" + 0);
+					 System.out.println("x=" + x + " y=" + y + " label=" + 0);
 				} else {
-					boolean noLabelledNeighbours = !pixelMatrix[y - 1][x]
-							&& !pixelMatrix[y][x - 1];
+					boolean noLabelledNeighbours = !pixel(pixelMatrix, x, y - 1)
+							&& !pixel(pixelMatrix, x - 1, y);
 					if (diagonalNeighbours) {
 						noLabelledNeighbours = noLabelledNeighbours
-								&& !pixelMatrix[y - 1][x - 1]
-								&& !pixelMatrix[y - 1][x + 1];
+								&& !pixel(pixelMatrix, x - 1, y - 1)
+								&& !pixel(pixelMatrix, x + 1, y - 1);
 					}
 					if (noLabelledNeighbours) {
 						// System.out.println("x=" + x + " y=" + y + " label=" +
 						// label);
 						labels[y][x] = label++;
 					} else {
-						int min = min(labels[y - 1][x], labels[y][x - 1]);
+						int min = min(label(labels, x, y - 1),
+								label(labels, x - 1, y));
 						if (diagonalNeighbours) {
-							min = min(min, labels[y - 1][x - 1],
-									labels[y - 1][x + 1]);
+							min = min(min, label(labels, x - 1, y - 1),
+									label(labels, x + 1, y - 1));
 						}
 						labels[y][x] = min;
 						// System.out.println("x=" + x + " y=" + y + " label=" +
@@ -59,22 +58,28 @@ public final class Multipass {
 				}
 			}
 		}
+		System.out.println("lol");
 
 		boolean labelChanged = false;
 		do {
+			//System.out.println("lol");
 			labelChanged = false;
 			for (int y = 0; y < height; y++) {
 				for (int x = 0; x < width; x++) {
 					if (pixelMatrix[y][x]) {
-						int min = min(labels[y - 1][x], labels[y][x - 1],
-								labels[y][x + 1], labels[y + 1][x + 1]);
+						int min = min(label(labels, x, y - 1),
+								label(labels, x - 1, y),
+								label(labels, x, y),
+								label(labels, x + 1, y),
+								label(labels, x, y + 1));
 						if (diagonalNeighbours) {
-							min = min(min, labels[y - 1][x - 1],
-									labels[y - 1][x + 1], labels[y + 1][x - 1],
-									labels[y + 1][x + 1]);
+							min = min(min, label(labels, x - 1, y - 1),
+									label(labels, x + 1, y - 1),
+									label(labels, x - 1, x + 1),
+									label(labels, x + 1, y + 1));
 						}
 
-						if (min != 0) {
+						if (min != label(labels,x,y)) {
 							labels[y][x] = min;
 							labelChanged = true;
 							// System.out.println("ch: x=" + x + " y=" + y +
@@ -83,7 +88,7 @@ public final class Multipass {
 					}
 				}
 			}
-		} while (!labelChanged);
+		} while (labelChanged);
 
 		// List<Blob> blobs = new ArrayList<Blob>();
 		// Set<Integer> remainingLabels = new HashSet(labels.values());
@@ -103,10 +108,28 @@ public final class Multipass {
 		return labels;
 	}
 
-	private static int min(Integer... values) {
+	private static boolean pixel(boolean[][] pixelMatrix, int x, int y) {
+		boolean pixel = false;
+		try {
+			pixel = pixelMatrix[y][x];
+		} catch (ArrayIndexOutOfBoundsException e) {
+		}
+		return pixel;
+	}
+
+	private static int label(int[][] labelMatrix, int x, int y) {
+		int label = 0;
+		try {
+			label = labelMatrix[y][x];
+		} catch (ArrayIndexOutOfBoundsException e) {
+		}
+		return label;
+	}
+
+	private static int min(int... values) {
 		int min = 0;
-		for (Integer value : values) {
-			if (value != null && value != 0) {
+		for (int value : values) {
+			if (value != 0) {
 				if (min == 0) {
 					min = value;
 				} else {
@@ -116,4 +139,30 @@ public final class Multipass {
 		}
 		return min;
 	}
+
+//	final static boolean[][] testImage = {
+//			{ false, false, false, true, true, false, false, false, true,
+//					false, true, false },
+//			{ true, true, true, false, true, true, true, true, false, false,
+//					false, true },
+//			{ true, true, true, false, true, false, false, true, false, false,
+//					true, false },
+//			{ true, false, false, false, false, false, false, false, false,
+//					true, false, false } };
+//	final static int testHeight = 4;
+//	final static int testWidth = 12;
+
+//	final static boolean[][] testImage={{}};
+//	final static int testHeight=0;
+//	final static int testWidth=0;
+	
+//	public static void main(String[] args) {
+//		int[][] labelMatrix = labelledMatrix8(testWidth, testHeight, testImage);
+//		for (int y = 0; y < testHeight; y++) {
+//			for (int x = 0; x < testWidth; x++) {
+//				System.out.print(labelMatrix[y][x]);
+//			}
+//			System.out.println();
+//		}
+//	}
 }
