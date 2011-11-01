31e37d51c912936c2514afa14b2ecc24fee63342
Till Theis <till_theis2@yahoo.de>
2011-11-01 11:41:14 +0100

    add documentation for factory methods

--- src/adp2/implementations/BinaryImages.java
+++ src/adp2/implementations/BinaryImages.java
@@ -8,14 +8,31 @@ public final class BinaryImages {
 	private BinaryImages() {		
 	}
 	
+	/**
+	 * create a Point
+	 * 
+	 * @param x x-coordinate
+	 * @param y y-coordinate
+	 * @return point at (x, y)
+	 */
 	public static Point point(int x, int y) {
 		return PointImpl.valueOf(x,y);
 	}
 	
+	/**
+	 * create an invalid BinaryImage
+	 * 
+	 * @return Not a BinaryImage
+	 */
 	public static BinaryImage NaBI() {
 	    return NaBI.valueOf();
 	}
 	
+	/**
+	 * create an invalid Blob
+	 * 
+	 * @return Not a Blob
+	 */
 	public static Blob NaB() {
 	    return NaB.valueOf();
 	}
