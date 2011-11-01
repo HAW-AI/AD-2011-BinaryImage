1d5f311c5df6f6879a1cd7e802a562cf6b45783f
panos <panagiotis.filippidis@haw-hamburg.de>
2011-11-01 20:53:59 +0100

    equals und hashCode in PixelImpl hinzugefügt

--- src/adp2/implementations/PointImpl.java
+++ src/adp2/implementations/PointImpl.java
@@ -33,4 +33,29 @@ public class PointImpl implements Point {
 		return new PointImpl(x,y);
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Point))
+			return false;
+		Point other = (Point) obj;
+		if (x != other.x())
+			return false;
+		if (y != other.y())
+			return false;
+		return true;
+	}
+
 }
