2cf5dcb165e281eac99ec52e1d48d8be980e68c5
Till Theis <till_theis2@yahoo.de>
2011-11-01 11:38:06 +0100

    implement NaBI factory method

--- src/adp2/implementations/BinaryImages.java
+++ src/adp2/implementations/BinaryImages.java
@@ -1,5 +1,7 @@
 package adp2.implementations;
 
+import adp2.interfaces.BinaryImage;
+
 // Factory class
 public final class BinaryImages {
 	private BinaryImages() {		
@@ -7,5 +9,9 @@ public final class BinaryImages {
 	
 	public static Point point(int x, int y) {
 		return PointImpl.valueOf(x,y);
+	}
+	
+	public static BinaryImage NaBI() {
+	    return NaBI.valueOf();
 	}
 }
