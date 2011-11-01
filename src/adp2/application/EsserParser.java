65c74c9bb31bcfb69f11a79578d1753b94067a19
Ben <salzig+github@gmail.com>
2011-11-01 13:38:45 +0100

    fix broken String handling

--- src/adp2/application/EsserParser.java
+++ src/adp2/application/EsserParser.java
@@ -14,7 +14,6 @@ import java.util.List;
  * @author Patrick Detlefsen <patrick.detlefsen@haw-hamburg.de>
  */
 public class EsserParser {
-	private static final String LINE_DELIMITER = "\n";
 	private static final String ELEMENT_DELIMITER = " ";
 	
 	public static EsserImage parse(String file) {
@@ -36,24 +35,15 @@ public class EsserParser {
 		}
 		return result;
 	}
-	
-	/**
-	 * split string into string array by line breaks
-	 * @param body
-	 * @return string array
-	 */
-	private static String[] lines(String body) {
-		return body.split(LINE_DELIMITER);
-	}
 
 	/**
 	 * build "physical" image out of an String 2d array
 	 * @param body String representation of an 2d array
 	 */
-	private static boolean[][] parse_array(String body) {
-		boolean[][] result = new boolean[lines(body).length][];
+	private static boolean[][] parse_array(String[] body) {
+		boolean[][] result = new boolean[body.length][];
 		int counter = 0;
-		for (String line : lines(body)) {
+		for (String line : body) {
 			result[counter] = elements(line);
 			counter++; // never ever touch this line, else this shit will break
 		}
@@ -64,12 +54,8 @@ public class EsserParser {
 	 * return the acsii image string, without header
 	 * @param file
 	 */
-	private static String body(String[] content) {
-		StringBuilder result = new StringBuilder();
-		for (String line : Arrays.copyOfRange(content, 1, content.length)) {
-			result.append(line);
-		}
-		return result.toString();
+	private static String[] body(String[] content) {
+		return Arrays.copyOfRange(content, 1, content.length);
 	}
 	
 	/**
