d3eacb112754f0fe5e53ffe553f1e2787641c4e9
panos <panagiotis.filippidis@haw-hamburg.de>
2011-11-01 12:09:52 +0100

    Neue Bugs gefunden und entfernt

--- src/adp2/implementations/BinaryImagez.java
+++ src/adp2/implementations/BinaryImagez.java
@@ -1,5 +1,10 @@
 package adp2.implementations;
 
+import java.util.Collection;
+
+import adp2.interfaces.Point;
+import adp2.interfaces.Blob;
+
 // Factory class
 public final class BinaryImagez {
 	private BinaryImagez() {		
@@ -7,5 +12,9 @@ public final class BinaryImagez {
 	
 	public static Point point(int x, int y) {
 		return PointImpl.valueOf(x,y);
+	}
+
+	static Blob blob(Collection<Point> points) {
+		return BlobImpl.valueOf(points);
 	}
 }
--- src/adp2/label/Multipass.java
+++ src/adp2/label/Multipass.java
@@ -1,27 +1,39 @@
 package adp2.label;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import adp2.interfaces.Blob;
+import adp2.interfaces.Point;
+import static adp2.implementations.BinaryImagez.point;
+
 public final class Multipass {
 	private Multipass() {
 
 	}
 
-	public static int[][] labelledMatrix4(int width, int height,
+	public static List<Blob> blobs4(int width, int height,
 			boolean[][] pixelMatrix) {
-		return labelledMatrix(width, height, pixelMatrix, false);
+		return blobs(width, height, pixelMatrix, false);
 	}
 
-	public static int[][] labelledMatrix8(int width, int height,
+	public static List<Blob> blobs8(int width, int height,
 			boolean[][] pixelMatrix) {
-		return labelledMatrix(width, height, pixelMatrix, true);
+		return blobs(width, height, pixelMatrix, true);
 	}
 
-	private static int[][] labelledMatrix(int width, int height,
+	private static List<Blob> blobs(int width, int height,
 			boolean[][] pixelMatrix, boolean diagonalNeighbours) {
-		if (pixelMatrix.length==0) return null;
 		// if (pixels.length == 0 || pixels[0].length == 0) {
 		// return null;
 		// }
-		int[][] labels = new int[height][width];
+		Map<Point, Integer> labels = new HashMap<Point, Integer>();
 
 		// boolean diagonalNeighbours = true;
 
@@ -29,28 +41,28 @@ public final class Multipass {
 		for (int y = 0; y < height; y++) {
 			for (int x = 0; x < width; x++) {
 				if (!pixelMatrix[y][x]) {
-					labels[y][x] = 0;
-//					 System.out.println("x=" + x + " y=" + y + " label=" + 0);
+					labels.put(point(x, y), 0);
+//					System.out.println("x=" + x + " y=" + y + " label=" + 0);
 				} else {
-					boolean noLabelledNeighbours = !pixel(pixelMatrix, x, y - 1)
+					boolean noForegroundNeighbours = !pixel(pixelMatrix, x, y - 1)
 							&& !pixel(pixelMatrix, x - 1, y);
 					if (diagonalNeighbours) {
-						noLabelledNeighbours = noLabelledNeighbours
+						noForegroundNeighbours = noForegroundNeighbours
 								&& !pixel(pixelMatrix, x - 1, y - 1)
 								&& !pixel(pixelMatrix, x + 1, y - 1);
 					}
-					if (noLabelledNeighbours) {
-						// System.out.println("x=" + x + " y=" + y + " label=" +
-						// label);
-						labels[y][x] = label++;
+					if (noForegroundNeighbours) {
+						 System.out.println("x=" + x + " y=" + y + " label=" +
+						 label);
+						labels.put(point(x, y), label++);
 					} else {
-						int min = min(label(labels, x, y - 1),
-								label(labels, x - 1, y));
+						int min = min(labels.get(point(x, y - 1)),
+								labels.get(point(x - 1, y)));
 						if (diagonalNeighbours) {
-							min = min(min, label(labels, x - 1, y - 1),
-									label(labels, x + 1, y - 1));
+							min = min(min, labels.get(point(x - 1, y - 1)),
+									labels.get(point(x + 1, y - 1)));
 						}
-						labels[y][x] = min;
+						labels.put(point(x, y), min);
 						// System.out.println("x=" + x + " y=" + y + " label=" +
 						// labels.get(pixel(x, y)));
 					}
@@ -64,45 +76,53 @@ public final class Multipass {
 			for (int y = 0; y < height; y++) {
 				for (int x = 0; x < width; x++) {
 					if (pixelMatrix[y][x]) {
-						int min = min(label(labels, x, y - 1),
-								label(labels, x - 1, y),
-								label(labels, x, y),
-								label(labels, x + 1, y),
-								label(labels, x, y + 1));
+						int min = min(labels.get(point(x, y - 1)),
+								labels.get(point(x - 1, y)),
+								labels.get(point(x, y)),
+								labels.get(point(x + 1, y)),
+								labels.get(point(x, y + 1)));
 						if (diagonalNeighbours) {
-							min = min(min, label(labels, x - 1, y - 1),
-									label(labels, x + 1, y - 1),
-									label(labels, x - 1, y + 1),
-									label(labels, x + 1, y + 1));
+							min = min(min, labels.get(point(x - 1, y - 1)),
+									labels.get(point(x + 1, y - 1)),
+									labels.get(point(x - 1, y + 1)),
+									labels.get(point(x + 1, y + 1)));
 						}
-
-						if (min != label(labels,x,y)) {
-							labels[y][x] = min;
+						if (min != labels.get(point(x, y))) {
+							labels.put(point(x, y), min);
 							labelChanged = true;
-							// System.out.println("ch: x=" + x + " y=" + y +
-							// " label=" +min);
+//							 System.out.println("ch: x=" + x + " y=" + y +
+//							 " label=" +min);
 						}
 					}
 				}
 			}
 		} while (labelChanged);
 
-		// List<Blob> blobs = new ArrayList<Blob>();
-		// Set<Integer> remainingLabels = new HashSet(labels.values());
-		// remainingLabels.remove(0);
-		// for (int currentLabel : remainingLabels) {
-		// SortedSet<Pixel> blobPixels = new TreeSet<Pixel>();
-		// for (Pixel pixel : pixels) {
-		// if (labels.get(pixel) == currentLabel) {
-		// blobPixels.add(pixel);
-		// }
-		// }
-		// blobs.add(adp2.adt.BinaryBlobz.blob(8, blobPixels));
-		// }
-		//
-		// return blobs;
+		for (int y=0; y<height; y++) {
+			for (int x=0; x<width; x++) {
+				System.out.print(labels.get(point(x,y)));
+			}
+			System.out.println();
+		}
+		System.out.println("end");
+		
+		
+		List<Blob> blobs = new ArrayList<Blob>();
+		Set<Integer> remainingLabels = new HashSet<Integer>(labels.values());
+		remainingLabels.remove(0);
+		for (int currentLabel : remainingLabels) {
+			SortedSet<Point> blobPoints = new TreeSet<Point>();
+			for (Point point : blobPoints) {
+				if (labels.get(point) == currentLabel) {
+					blobPoints.add(point);
+				}
+			}
+			blobs.add(adp2.implementations.BinaryImagez.blob(blobPoints));
+		}
+		
+		return blobs;
 
-		return labels;
+		// return labels;
 	}
 
 	private static boolean pixel(boolean[][] pixelMatrix, int x, int y) {
@@ -114,19 +134,19 @@ public final class Multipass {
 		return pixel;
 	}
 
-	private static int label(int[][] labelMatrix, int x, int y) {
-		int label = 0;
-		try {
-			label = labelMatrix[y][x];
-		} catch (ArrayIndexOutOfBoundsException e) {
-		}
-		return label;
-	}
+//	private static int label(int[][] labelMatrix, int x, int y) {
+//		int label = 0;
+//		try {
+//			label = labelMatrix[y][x];
+//		} catch (ArrayIndexOutOfBoundsException e) {
+//		}
+//		return label;
+//	}
 
-	private static int min(int... values) {
+	private static int min(Integer... values) {
 		int min = 0;
-		for (int value : values) {
-			if (value != 0) {
+		for (Integer value : values) {
+			if (value != null && value != 0) {
 				if (min == 0) {
 					min = value;
 				} else {
@@ -137,29 +157,52 @@ public final class Multipass {
 		return min;
 	}
 
-//	final static boolean[][] testImage = {
-//			{ false, false, false, true, true, false, false, false, true,
-//					false, true, false },
-//			{ true, true, true, false, true, true, true, true, false, false,
-//					false, true },
-//			{ true, true, true, false, true, false, false, true, false, false,
-//					true, false },
-//			{ true, false, false, false, false, false, false, false, false,
-//					true, false, false } };
-//	final static int testHeight = 4;
-//	final static int testWidth = 12;
-
-//	final static boolean[][] testImage={{}};
-//	final static int testHeight=0;
-//	final static int testWidth=0;
-	
-//	public static void main(String[] args) {
-//		int[][] labelMatrix = labelledMatrix4(testWidth, testHeight, testImage);
-//		for (int y = 0; y < testHeight; y++) {
-//			for (int x = 0; x < testWidth; x++) {
-//				System.out.print(labelMatrix[y][x]);
+//	public static Collection<Blob> blob4(int width, int height,
+//			boolean[][] pixelMatrix) {
+//		int[][] labelledMatrix = labelledMatrix4(width, height, pixelMatrix);
+//		return null;
+//	}
+//
+//	private Collection<Blob> blobs(int[][] labelledMatrix) {
+//		List<Blob> blobs = new ArrayList<Blob>();
+//		Set<Integer> remainingLabels = new HashSet(labels.values());
+//		remainingLabels.remove(0);
+//		for (int currentLabel : remainingLabels) {
+//			SortedSet<Pixel> blobPixels = new TreeSet<Pixel>();
+//			for (Pixel pixel : pixels) {
+//				if (labels.get(pixel) == currentLabel) {
+//					blobPixels.add(pixel);
+//				}
 //			}
-//			System.out.println();
+//			blobs.add(adp2.adt.BinaryBlobz.blob(8, blobPixels));
 //		}
+//
+//		return blobs;
+//
+//	}
+//	 final static boolean[][] testImage = {
+//	 { false, false, false, true, true, false, false, false, true,
+//	 false, true, false },
+//	 { true, true, true, false, true, true, true, true, false, false,
+//	 false, true },
+//	 { true, true, true, false, true, false, false, true, false, false,
+//	 true, false },
+//	 { true, false, false, false, false, false, false, false, false,
+//	 true, false, false } };
+//	 final static int testHeight = 4;
+//	 final static int testWidth = 12;
+
+	// final static boolean[][] testImage={{}};
+	// final static int testHeight=0;
+	// final static int testWidth=0;
+
+//	 public static void main(String[] args) {
+//	 blobs8(testWidth, testHeight, testImage);
+	// for (int y = 0; y < testHeight; y++) {
+	// for (int x = 0; x < testWidth; x++) {
+	// System.out.print(labelMatrix[y][x]);
+	// }
+	// System.out.println();
+	// }
 //	}
 }
