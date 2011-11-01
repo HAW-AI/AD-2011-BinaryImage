e0a72bcc34f2873c6a3ee28b554ae79a4e4f3ce5
Ben <salzig+github@gmail.com>
2011-11-01 12:46:41 +0100

    yeah, controller is done

--- src/adp2/application/Controller.java
+++ src/adp2/application/Controller.java
@@ -1,7 +1,8 @@
-/**
- * 
- */
 package adp2.application;
+
+import java.io.File;
+import adp2.implementations.BinaryImages;
+import adp2.interfaces.BinaryImage;
 
 /**
  * @author Ben Rexin <benjamin.rexin@haw-hamburg.de>
@@ -10,7 +11,74 @@ package adp2.application;
  */
 public class Controller {
 	public static void main(String[] args) {
-		
+		if (new File(args[0]).canRead()) {
+			new Controller(args[0]);
+		}
+		else {
+			new Controller();
+		}
 	}
-
+	
+	/**
+	 * Mutable, else we would need to restart for opening a image
+	 */
+	private BinaryImage binaryImage;
+	
+	/**
+	 * Constructor builds BinaryImage for given File
+	 * @param filename
+	 */
+	private Controller(String filename) {
+		this();
+		setBinaryImage(openImage(filename));
+	}
+	
+	/**
+	 * Handles basic object init.
+	 */
+	private Controller() {
+		// no file given, what now?
+		// - create view
+		// - keep running
+	}
+	
+	/**
+	 * accessor method for retrieving binaryImage 
+	 * @return BinaryImage
+	 */
+	public BinaryImage binaryImage() {
+		return this.binaryImage;
+	}
+	
+	/**
+	 * open file and build EsserImage
+	 * @param filename
+	 * @return
+	 */
+	public EsserImage openImage(String filename) {
+		return EsserParser.parse(filename);
+	}
+	
+	/**
+	 * set binaryImage field to given BinaryImage, after converting from EsserImage
+	 * @param image
+	 */
+	private void setBinaryImage(EsserImage image) {
+		setBinaryImage(BinaryImages.binaryImage(image));
+	}
+	
+	/**
+	 * set binaryImage field to given BinaryImage
+	 * @param image
+	 */
+	private void setBinaryImage(BinaryImage image) {
+		this.binaryImage = image;
+	}
+	/**
+	 * predicate method indicating BinaryImage presents
+	 * @return
+	 */
+	public boolean isBinaryImageAvailable() {
+		return this.binaryImage == null;
+	}
 }
