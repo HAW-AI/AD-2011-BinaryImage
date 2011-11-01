8fdf56f94a4962b74d173cac2a524dffbdaf3d4b
Ben <salzig+github@gmail.com>
2011-11-01 12:19:26 +0100

    add factory for binaryImage

--- src/adp2/implementations/BinaryImages.java
+++ src/adp2/implementations/BinaryImages.java
@@ -1,80 +1,97 @@
 package adp2.implementations;
 
 import java.util.List;
-
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import adp2.application.EsserImage;
 import adp2.interfaces.BinaryImage;
 import adp2.interfaces.Blob;
 
 // Factory class
 public final class BinaryImages {
-	private BinaryImages() {		
+	private BinaryImages() {
 	}
-	
+
 	/**
 	 * create a Point
 	 * 
-	 * @param x x-coordinate
-	 * @param y y-coordinate
+	 * @param x
+	 *            x-coordinate
+	 * @param y
+	 *            y-coordinate
 	 * @return point at (x, y)
 	 */
 	public static Point point(int x, int y) {
-		return PointImpl.valueOf(x,y);
+		return PointImpl.valueOf(x, y);
 	}
-	
+
 	/**
 	 * create an invalid BinaryImage
 	 * 
 	 * @return Not a BinaryImage
 	 */
 	public static BinaryImage NaBI() {
-	    return NaBI.valueOf();
+		return NaBI.valueOf();
 	}
-	
+
 	/**
 	 * create an invalid Blob
 	 * 
 	 * @return Not a Blob
 	 */
 	public static Blob NaB() {
-	    return NaB.valueOf();
+		return NaB.valueOf();
 	}
-	
+
 	public static BinaryImage fourNeighborBinaryImage(List<List<Boolean>> shape) {
-	    BinaryImage img;
-	    
-	    if (isValidBinaryImageShape(shape)) {
-	        img = FourNeighborBinaryImage.valueOf(shape);
-	    } else {
-	        img = NaBI();
-	    }
-	    
-	    return img;
+		BinaryImage img;
+
+		if (isValidBinaryImageShape(shape)) {
+			img = FourNeighborBinaryImage.valueOf(shape);
+		} else {
+			img = NaBI();
+		}
+
+		return img;
 	}
-	
+
 	public static BinaryImage eightNeighborBinaryImage(List<List<Boolean>> shape) {
-        BinaryImage img;
-        
-        if (isValidBinaryImageShape(shape)) {
-            img = EightNeighborBinaryImage.valueOf(shape);
-        } else {
-            img = NaBI();
-        }
-        
-        return img;
+		BinaryImage img;
+
+		if (isValidBinaryImageShape(shape)) {
+			img = EightNeighborBinaryImage.valueOf(shape);
+		} else {
+			img = NaBI();
+		}
+
+		return img;
 	}
-	
+
 	private static boolean isValidBinaryImageShape(List<List<Boolean>> shape) {
-	    if (shape == null) return false;
-	    
-	    if (shape.size() == 0) return true;
-	    if (shape.get(0) == null) return false;
-	    
-	    int width = shape.get(0).size();
-	    
-	    for (List<Boolean> row : shape) {
-	        if (row == null || row.size() != width) return false;
-	    }
-	    
-	    return true;
+		if (shape == null)
+			return false;
+
+		if (shape.size() == 0)
+			return true;
+		if (shape.get(0) == null)
+			return false;
+
+		int width = shape.get(0).size();
+
+		for (List<Boolean> row : shape) {
+			if (row == null || row.size() != width)
+				return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * factory methods getting EsserImage and retuns BinaryImage
+	 * 
+	 * @param image
+	 * @return
+	 */
+	public static BinaryImage binaryImage(EsserImage image) {
+		throw new NotImplementedException();
 	}
 }
