53a99d80c258154098d347f6feb0320d0d7b5918
Felix1337 <felix.schmidt@haw-hamburg.de>
2011-11-01 11:20:03 +0100

    BlobImpl ist da

--- src/adp2/implementations/BlobImpl.java
+++ src/adp2/implementations/BlobImpl.java
@@ -44,4 +44,29 @@ public class BlobImpl implements Blob {
 		return max-min;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((s == null) ? 0 : s.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BlobImpl other = (BlobImpl) obj;
+		if (s == null) {
+			if (other.s != null)
+				return false;
+		} else if (!s.equals(other.s))
+			return false;
+		return true;
+	}
+
 }
