611acd50992cf7eb103af19ac72691c2e4636959
Raimund <Raimund@raimunds-macbook.local>
2011-11-01 11:16:46 +0100

    implement NaB

--- src/adp2/implementations/NaB.java
+++ src/adp2/implementations/NaB.java
@@ -1,33 +1,58 @@
 package adp2.implementations;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import adp2.interfaces.Blob;
 import adp2.interfaces.Point;
 
 final class NaB implements Blob {
-
+	
+	private final static NaB instance = new NaB();
+	
+	private NaB() {}
+	
+	/**
+	 * 
+	 * @return the instance of NaB
+	 */
+	public static Blob valueOf() {
+		return instance;
+	}
+	
+	/**
+	 * 
+	 * @return an iterator of an empty collection of Point
+	 */
 	@Override
 	public Iterator<Point> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return (new ArrayList<Point>()).iterator();
 	}
 
+	/**
+	 * 
+	 * @return number of Points 
+	 */
 	@Override
 	public int pointCount() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	/**
+	 * 
+	 * @return width of the Blob
+	 */
 	@Override
 	public int width() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	/**
+	 * 
+	 * @return height of Blob
+	 */
 	@Override
 	public int height() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
