7bea00f451e295e2ab3e418d20cb608467757070
Oliver <oliver.behncke@googlemail.com>
2011-11-03 19:35:02 +0100

    Removed unneeded import in AbstractBI

--- src/adp2/implementations/AbstractBinaryImage.java
+++ src/adp2/implementations/AbstractBinaryImage.java
@@ -3,7 +3,6 @@ package adp2.implementations;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
