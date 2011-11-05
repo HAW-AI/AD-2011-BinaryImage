ef437149759ba996f80d792a1568360eb5bf932d
Neonchen <neongen@web.de>
2011-11-05 15:08:43 +0100

    Correction: BinaryImage added to match changed blob implementation

--- src/adp2/methodTests/AxiomsTest.java
+++ src/adp2/methodTests/AxiomsTest.java
@@ -1,6 +1,7 @@
 package adp2.methodTests;
 
 import static adp2.implementations.BinaryImages.*;
+import static adp2.application.EsserParser.*;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
@@ -9,13 +10,15 @@ import java.util.TreeSet;
 
 import org.junit.Test;
 
+import adp2.implementations.BinaryImages;
+import adp2.interfaces.BinaryImage;
 import adp2.interfaces.Blob;
 import adp2.interfaces.Point;
 
 public class AxiomsTest {
-	
+	BinaryImage Bi = BinaryImages.binaryImage(parse("test/fixtures/32x32.esser"));
 	Point p1 = point(3,1);
-	Blob b1 = blob(Arrays.asList(p1,point(4,1),point(3,2)));
+	Blob b1 = blob(Arrays.asList(p1,point(4,1),point(3,2)),Bi);
 	Set<Point> es = new TreeSet<Point>();
 	Blob nab1 = NaB();
 	
