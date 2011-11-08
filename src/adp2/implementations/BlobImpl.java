df66c615f1fbf9663db999d7d81b877ba6394573
Stephan <stephan.berngruber@haw-hamburg.de>
2011-11-08 15:35:21 +0100

    variable s zu pointsOfBlob umbenannt - da völlig sinnfreier name gewählt

--- src/adp2/implementations/BlobImpl.java
+++ src/adp2/implementations/BlobImpl.java
@@ -9,7 +9,7 @@ import adp2.interfaces.*;
 
 public class BlobImpl implements Blob {
 
-	private final TreeSet<Point> s;
+	private final TreeSet<Point> pointsOfBlob;
 	private final BinaryImage binaryImage;
 	private final double circularity;
 
@@ -27,11 +27,11 @@ public class BlobImpl implements Blob {
 	/**
 	 * Blob-Konstruktor.
 	 * 
-	 * @param s
+	 * @param pointsOfBlob
 	 *            : Collection, die alle Points des Blobs enthält.
 	 */
-	private BlobImpl(Collection<Point> s, BinaryImage image) {
-		this.s = new TreeSet<Point>(s);
+	private BlobImpl(Collection<Point> pointsOfBlob, BinaryImage image) {
+		this.pointsOfBlob = new TreeSet<Point>(pointsOfBlob);
 		this.binaryImage = image;
 		this.circularity = 4 * Math.PI * pointCount()
 				/ Math.pow(perimeter(), 2);
@@ -41,29 +41,29 @@ public class BlobImpl implements Blob {
 	 * Gibt einen Iterator über die Menge der Points des Blobs zurück.
 	 */
 	public Iterator<Point> iterator() {
-		return this.s.iterator();
+		return this.pointsOfBlob.iterator();
 	}
 
 	/**
 	 * Gibt die alle Points eines Blobs als Set zurueck.
 	 */
 	public Set<Point> points() {
-		return this.s;
+		return this.pointsOfBlob;
 	}
 
 	/**
 	 * Gibt die Größe des Blobs als int zurück.
 	 */
 	public int pointCount() {
-		return this.s.size();
+		return this.pointsOfBlob.size();
 	}
 
 	/**
 	 * Gibt die Breite des Blobs als int zurück.
 	 */
 	public int width() {
-		int max = s.first().x(), min = s.first().x();
-		for (Point p : s) {
+		int max = pointsOfBlob.first().x(), min = pointsOfBlob.first().x();
+		for (Point p : pointsOfBlob) {
 			if (p.x() < min) {
 				min = p.x();
 			}
@@ -78,8 +78,8 @@ public class BlobImpl implements Blob {
 	 * Gibt die Hoehe des Blobs als int zurück.
 	 */
 	public int height() {
-		int max = s.first().y(), min = s.first().y();
-		for (Point p : s) {
+		int max = pointsOfBlob.first().y(), min = pointsOfBlob.first().y();
+		for (Point p : pointsOfBlob) {
 			if (p.y() < min) {
 				min = p.y();
 			}
@@ -97,7 +97,7 @@ public class BlobImpl implements Blob {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((s == null) ? 0 : s.hashCode());
+		result = prime * result + ((pointsOfBlob == null) ? 0 : pointsOfBlob.hashCode());
 		return result;
 	}
 
@@ -116,10 +116,10 @@ public class BlobImpl implements Blob {
 		if (getClass() != obj.getClass())
 			return false;
 		BlobImpl other = (BlobImpl) obj;
-		if (s == null) {
-			if (other.s != null)
+		if (pointsOfBlob == null) {
+			if (other.pointsOfBlob != null)
 				return false;
-		} else if (!s.equals(other.s))
+		} else if (!pointsOfBlob.equals(other.pointsOfBlob))
 			return false;
 		return true;
 	}
@@ -127,12 +127,12 @@ public class BlobImpl implements Blob {
 	public boolean contains(Point p) {
 		if (p == null)
 			return false;
-		return s.contains(p);
+		return pointsOfBlob.contains(p);
 	}
 
 	@Override
 	public String toString() {
-		return this.s.toString();
+		return this.pointsOfBlob.toString();
 	}
 
 	@Override
@@ -165,7 +165,7 @@ public class BlobImpl implements Blob {
 			count = 4;
 		}
 
-		for (Point p : s) {
+		for (Point p : pointsOfBlob) {
 			// System.out.println("Neighbours" +binaryImage.neighbours(p) +
 			// " p: " + p);
 
